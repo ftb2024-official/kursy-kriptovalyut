3dbd7118ea05f5e2309cf63a9b4ecde61d019cf9
ftb2024_official <ftb2024@gmail.com>
2024-11-08 22:51:36 +0500

    created coin struct & constructor for it

--- internal/entity/coin.go
+++ internal/entity/coin.go
@@ -1,1 +1,29 @@
 package entity
+
+import "time"
+
+type Coin struct {
+	title    string
+	price    float64
+	actualAt time.Time
+}
+
+func NewCoin(title string, price float64) *Coin {
+	coin := &Coin{}
+
+	if title == "" {
+		coin.title = "BTC"
+	} else {
+		coin.title = "ETH"
+	}
+
+	if price == 0 {
+		coin.price = 0.0
+	} else {
+		coin.price = price
+	}
+
+	coin.actualAt = time.Now()
+
+	return coin
+}
