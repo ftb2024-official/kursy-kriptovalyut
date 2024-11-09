1f7da2d2d480a2c8338852d0408e3ee58378e5fd
ftb2024_official <ftb2024@gmail.com>
2024-11-09 14:32:22 +0500

    wrote tests for newcoin function

---
+++ internal/entity/coin_test.go
@@ -0,0 +1,58 @@
+package entity
+
+import (
+	"testing"
+	"time"
+)
+
+type newCoinTest struct {
+	arg1 string
+	arg2 float64
+	res1 string
+	res2 float64
+}
+
+var newCoinTests = []newCoinTest{
+	{"", 0, "BTC", 0},
+	{"BTC", -1, "BTC", 0},
+	{"ETH", 1, "ETH", 1},
+	{"USDT", 2, "BTC", 2},
+	{"smth else", 3, "BTC", 3},
+}
+
+func TestNewCoin(t *testing.T) {
+	for _, test := range newCoinTests {
+		out := NewCoin(test.arg1, test.arg2)
+		if out.title != test.res1 || out.price != test.res2 {
+			t.Errorf("got: %v, %v, want: %v, %v", out.title, out.price, test.res1, test.res2)
+		}
+
+		// Проверка, что время создания актуально (с небольшим допустимым отклонением)
+		if time.Since(out.actualAt) > time.Second {
+			t.Errorf("actualAt is too old: got %v", out.actualAt)
+		}
+	}
+}
+
+// for _, test := range []struct {
+// 	arg1 string
+// 	arg2 float64
+// 	res1 string
+// 	res2 float64
+// }{
+// 	{"", 0, "BTC", 0},
+// 	{"BTC", -1, "BTC", 0},
+// 	{"ETH", 1, "ETH", 1},
+// 	{"USDT", 2, "BTC", 2},
+// 	{"smth else", 3, "BTC", 3},
+// } {
+// 	out := NewCoin(test.arg1, test.arg2)
+// 	if out.title != test.res1 || out.price != test.res2 {
+// 		t.Errorf("got: %v, %v, want: %v, %v", out.title, out.price, test.res1, test.res2)
+// 	}
+
+// 	// Проверка, что время создания актуально (с небольшим допустимым отклонением)
+// 	if time.Since(out.actualAt) > time.Second {
+// 		t.Errorf("actualAt is too old: got %v", out.actualAt)
+// 	}
+// }
