e51452a6d9241f02133cc24276044b976ff2e533
ftb2024_official <ftb2024@gmail.com>
2024-11-30 13:59:40 +0500

    wrote crypto compare client

--- internal/adapters/provider/crypto_compare.go
+++ internal/adapters/provider/crypto_compare.go
@@ -2,17 +2,98 @@ package provider
 
 import (
 	"context"
-	entity "kursy-kriptovalyut/internal/entities"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"kursy-kriptovalyut/internal/entities"
 )
 
 type CryptoCompare struct {
-	apiKey string
+	baseUrl    string
+	apiKey     string
+	httpClient *http.Client
 }
 
-func NewCryptoCompare(apiKey string) *CryptoCompare {
-	return &CryptoCompare{apiKey: apiKey}
+func NewCryptoCompare(baseUrl string, apiKey string) (*CryptoCompare, error) {
+	if baseUrl == "" || apiKey == "" {
+		return nil, errors.Wrap(entities.ErrInvalidParam, "base-url/api-key is empty")
+	}
+
+	return &CryptoCompare{
+		baseUrl:    baseUrl,
+		apiKey:     apiKey,
+		httpClient: &http.Client{},
+	}, nil
 }
 
-func (cc *CryptoCompare) GetActualRates(ctx context.Context, titles []string) ([]entity.Coin, error) {
-	return nil, nil
+const (
+	fromSyms = "fsyms"
+	toSyms   = "tsyms"
+	currency = "USD"
+)
+
+func (cc *CryptoCompare) GetActualRates(ctx context.Context, titles []string) ([]entities.Coin, error) {
+	fUrl, err := url.Parse(cc.baseUrl)
+	if err != nil {
+		return nil, errors.Wrapf(entities.ErrInternal, "failed to parse url: %v", err)
+	}
+
+	// manual raw query
+	// rawQuery := fmt.Sprintf("%s=%s&%s=%s", fromSyms, strings.Join(titles, ","), toSyms, currency)
+	// fUrl.RawQuery = rawQuery
+
+	queries := fUrl.Query()
+	queries.Add(fromSyms, strings.Join(titles, ","))
+	queries.Add(toSyms, currency)
+
+	fUrl.RawQuery, err = url.QueryUnescape(queries.Encode())
+	if err != nil {
+		return nil, errors.Wrapf(entities.ErrInternal, "failed to encode url: %v", err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fUrl.String(), nil)
+	if err != nil {
+		return nil, errors.Wrapf(entities.ErrInternal, "failed to create new request, err: %v", err)
+	}
+
+	req.Header.Set("Authorization", "Apikey "+cc.apiKey)
+
+	resp, err := cc.httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrapf(entities.ErrInternal, "failed to execute request, err: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read response body")
+	}
+
+	var data map[string]map[string]float64
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse response body, invalid JSON format")
+	}
+
+	coins := make([]entities.Coin, 0, len(data))
+	for title, value := range data {
+		price := value[currency]
+
+		coin, err := entities.NewCoin(title, price)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to create new coin")
+		}
+
+		coins = append(coins, *coin)
+	}
+	return coins, nil
 }
